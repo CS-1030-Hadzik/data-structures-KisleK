1919f243ee83008d7e59b1a19d65acd38d573d0b
School <school@macbook-pro.lan>
2025-04-08 16:56:09 -0600

    finished problems 12-18

--- cars.py
+++ cars.py
@@ -21,15 +21,16 @@ cars.pop(4)
 # problem 11. print the array to the console
 print(cars)
 # problem 12. Sort the array in ascending order
-
+cars.sort()
 # problem 13. print the array to the console
-
+print(cars)
 # problem 14. Sort the array in descending order
-
+cars.sort(reverse=True)
 # problem 15. print the array to the console
-
+print(cars)
 # problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)
-
+my_array_length = len(cars)
 # problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)
-
+array_string = 'The length of my array is '
 # problem 18. print array_string concatenated with my_array_length to the console.
+print(array_string, my_array_length)
